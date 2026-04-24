d497821509c557cd39b1d0855eaee0f619ee703b
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-04-24 07:33:54 +0200

    working in main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+import { buscarPelicula } from "./api.js";
+import { mostrarPeliculas, historialPeliculas, actualizarContador } from "./ui.js";
+
+const movieInput = document.querySelector('#movie')
+const btnBuscar = document.querySelector('#btn-input')
+const resultado = document.querySelector('#resultado')
+
+btnBuscar.addEventListener('click', async function(){
+    if(movieInput.value){ //validar antes de petición
+        try {
+            resultado.textContent = 'Buscando ...'
+            const datos = await buscarPelicula(movieInput.value)
+
+            if(datos.error){
+                resultado.textContent = 'Esta pelicula no existe'
+            }else{
+                
+               const title =  datos.Title      // título de la película
+               const year = datos.Year       // año de estreno
+               const genre = datos.Genre      // género
+               const director =  datos.Director   // director
+               // datos.Response   // "True" si existe, "False" si no existe
+               mostrarPeliculas()
+               actualizarContador()
+            }
+            
+        } catch (error) {
+            resultado.textContent = 'Algo no ha ido bien '+ error
+        }   
+        }else{
+            resultado.textContent = 'Indica una Pelicula'
+        
+    
+    }
+})
 
 
 
@@ -6,11 +41,3 @@
 
 
 
-
-/**
- * datos.Title      // título de la película
-datos.Year       // año de estreno
-datos.Genre      // género
-datos.Director   // director
-datos.Response   // "True" si existe, "False" si no existe
- */
--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,13 @@
 const resultado = document.querySelector('#resultado')
 const lista = document.querySelector('#lista')
 
-export function mostrarPeliculas(Title, Year, Genre, Director){
-            resultado.textContent = `Pelicula: ${Title}, Año: ${Year}, Género: ${Genre}, Director: ${Director}`
+export function mostrarPeliculas(title, year, genre, director){
+            resultado.textContent = `Pelicula: ${title}, Año: ${year}, Género: ${genre}, Director: ${director}`
 }
 
-export function historialPeliculas(Title, Year, Genre, Director){
+export function historialPeliculas(title, year, genre, director){
     const nuevoLi = document.createElement('li')//creamos la lista
-    nuevoLi.textContent = `Pelicula: ${Title}, Año: ${Year}, Género: ${Genre}, Director: ${Director}`
+    nuevoLi.textContent = `Pelicula: ${title}, Año: ${year}, Género: ${genre}, Director: ${director}`
     const btnEliminar = document.createElement('button')
     btnEliminar.textContent = 'Eliminar'
     btnEliminar.addEventListener('click', function(){
